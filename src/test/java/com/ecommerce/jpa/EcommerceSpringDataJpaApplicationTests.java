ad5ff350a4ace177ac19ed3d4037925c24c6cdc9
Durga Prasad K <durga-prasad.kalamati@capgemini.com>
2024-11-11 10:25:31 +0530

    initial commit

---
+++ src/test/java/com/ecommerce/jpa/EcommerceSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
